b525c450a36b03164d1a355dfe14a219536eb3c2
mohanreddy592 <mohanreddy592@gmail.com>
2017-03-07 17:37:43 +0530

    added load balancing

--- Spring-Cloud-OrderMS/src/main/java/com/spring/cloud/service/TollService.java
+++ Spring-Cloud-OrderMS/src/main/java/com/spring/cloud/service/TollService.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,15 @@ public class TollService {
 
 	@RequestMapping(value = "/tolls", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<TollUsage> findAllTolls() {
-		return tollRepository.findAll();
+		List<TollUsage> tollUsages = new ArrayList<>();
+		tollUsages.addAll(tollRepository.findAll());
+		// remove hot code data before starting one instance
+		// put the changes and then start another server (not in debug mode)
+		// we will get different data from load balancing
+		tollUsages.add(new TollUsage("123", "R1", "AP04N8790", "05:51"));
+		tollUsages.add(new TollUsage("456", "R2", "AP05N6543", "07:03"));
+		tollUsages.add(new TollUsage("789", "R3", "AP06N1234", "09:36"));
+		return tollUsages;
 	}
 
 	@RequestMapping(value = "/tolls/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
